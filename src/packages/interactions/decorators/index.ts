dd890eebb24dcedc195958130a17bd2e3dd6ca9c
unknown <nikitakabo74@gmail.com>
2025-01-01 22:05:07 +0300

    fix: do not exported sub-command decorator

--- src/packages/interactions/decorators/index.ts
+++ src/packages/interactions/decorators/index.ts
@@ -3,3 +3,4 @@ export * from "./component.decorator";
 export * from "./event.decorator";
 export * from "./slash-command.decorator";
 export * from "./text-command.decorator";
+export * from "./sub-command.decorator";
