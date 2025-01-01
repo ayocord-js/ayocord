de237e5a694ee4d24d6deac8622db8caed17a8bb
unknown <nikitakabo74@gmail.com>
2025-01-01 14:56:35 +0300

    chore: client is optional

--- src/packages/modules/types/module.interface.ts
+++ src/packages/modules/types/module.interface.ts
@@ -1,6 +1,6 @@
 import { DiscordClient } from "@/packages/client";
 
 export interface DiscordModule {
-  onEnable?: (client: DiscordClient) => any | Promise<any>;
-  onDisable?: (client: DiscordClient) => any | Promise<any>;
+  onEnable?: (client?: DiscordClient) => any | Promise<any>;
+  onDisable?: (client?: DiscordClient) => any | Promise<any>;
 }
